a2220f63685ad2a5233094f57c32b6b1103eb8d5
anton <a.bondarenko@distillery.com>
2020-04-13 13:09:29 +0300

    fix test

--- first_test.py
+++ first_test.py
@@ -4,14 +4,17 @@ from selenium import webdriver
 import geckodriver_autoinstaller
 from selenium.webdriver.firefox.options import Options
 
-
 geckodriver_autoinstaller.install()  # Check if the current version of geckodriver exists
-                                     # and if it doesn't exist, download it automatically,
-                                     # then add geckodriver to path
-options = Options()
-options.headless = True
-options.add_argument('-headless')
-driver = webdriver.Firefox(options=options)
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-driver.quit()
+
+
+# and if it doesn't exist, download it automatically,
+
+
+def test_login_button():
+    options = Options()
+    options.headless = True
+    options.add_argument('-headless')
+    driver = webdriver.Firefox(options=options)
+    driver.get("http://www.python.org")
+    assert "Python" in driver.title
+    driver.quit()
