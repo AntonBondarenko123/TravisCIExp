e7cc7af49dd819c1ec80581a883df519ee3d877d
anton <a.bondarenko@distillery.com>
2020-04-13 12:59:17 +0300

    add headless

--- first_test.py
+++ first_test.py
@@ -2,14 +2,16 @@ from time import sleep
 
 from selenium import webdriver
 import geckodriver_autoinstaller
+from selenium.webdriver.firefox.options import Options
 
 
 geckodriver_autoinstaller.install()  # Check if the current version of geckodriver exists
                                      # and if it doesn't exist, download it automatically,
                                      # then add geckodriver to path
-
-driver = webdriver.Firefox()
+options = Options()
+options.headless = True
+options.add_argument('-headless')
+driver = webdriver.Firefox(options=options)
 driver.get("http://www.python.org")
 assert "Python" in driver.title
-sleep(7)
 driver.quit()
