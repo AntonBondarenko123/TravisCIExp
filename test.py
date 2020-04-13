0599ffae255e1e6a573c3a8b10aa51c9c2df313f
anton <a.bondarenko@distillery.com>
2020-04-13 12:12:01 +0300

    First steps

---
+++ test.py
@@ -0,0 +1,15 @@
+from time import sleep
+
+from selenium import webdriver
+import geckodriver_autoinstaller
+
+
+geckodriver_autoinstaller.install()  # Check if the current version of geckodriver exists
+                                     # and if it doesn't exist, download it automatically,
+                                     # then add geckodriver to path
+
+driver = webdriver.Firefox()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+sleep(7)
+driver.quit()
